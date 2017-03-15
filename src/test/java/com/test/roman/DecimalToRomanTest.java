dfc7aff613b81ef9464f1ad463172e8c8a4d3666
jaldine <jaldine@hotmail.com>
2017-03-15 06:36:33 +0100

    TDD comment test

--- src/test/java/com/test/roman/DecimalToRomanTest.java
+++ src/test/java/com/test/roman/DecimalToRomanTest.java
@@ -8,6 +8,7 @@ public class DecimalToRomanTest{
 	//TDD Decimal to roman
 	@Test
 	public void decimalToRomanTest(){
+		//assert that the return value of toRoman method is II
 		Assert.assertEquals(DecimalToRoman.toRoman(2), "II");
 	}
 
