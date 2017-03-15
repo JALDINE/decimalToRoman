f26dc5ceb6644a719fd04aad90dd364997b7b71d
jaldine <jaldine@hotmail.com>
2017-03-15 05:16:02 +0100

    TDD call toRoman method

--- src/test/java/com/test/roman/DecimalToRomanTest.java
+++ src/test/java/com/test/roman/DecimalToRomanTest.java
@@ -7,6 +7,7 @@ public class DecimalToRomanTest extends TestCase {
 	//TDD Decimal to roman
 	public void decimalToRomanTest(){
 		DecimalToRoman decimal=new DecimalToRoman();
+		String roman=decimal.toRoman(1);
 	}
 
 }
