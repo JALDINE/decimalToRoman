6be67b90e9dc7280ba28697c30ab5c5ae0b8bdbe
jaldine <jaldine@hotmail.com>
2017-03-15 06:29:03 +0100

    Correct the comment

--- src/main/java/com/test/roman/DecimalToRoman.java
+++ src/main/java/com/test/roman/DecimalToRoman.java
@@ -6,7 +6,7 @@ public class DecimalToRoman {
 	
 
 	/*create a map variable and attribute to each decimal his roman value.
-	 * this map will help us to find the roman value corresponding to a a decimal
+	 * this map will help us to find the roman value corresponding to a decimal
 	 */
 	 private final static TreeMap<Integer, String> map = new TreeMap<Integer, String>();
 
@@ -35,7 +35,7 @@ public class DecimalToRoman {
 	            return map.get(num);
 	        }
 	        /*else we get the roman value and we concatenate to the return value of the toRoman method 
-	        the call of toRomain is recursive until we find the key value equal to the parameter value  
+	        the call of toRoman is recursive until we find the key value equal to the parameter value  
 	        */
 	        return map.get(i) + toRoman(num-i);
 	    }
